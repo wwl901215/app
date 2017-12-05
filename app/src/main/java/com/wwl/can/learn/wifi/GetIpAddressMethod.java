434c88e47ceeed5e91fefbbde0447e30e54caca7
王文亮 <wb_wangwenliang@ocj.com.cn>
2017-12-05 20:01:30 +0800

    一个获取手机本身网络信号ip地址的方法；

---
+++ app/src/main/java/com/wwl/can/learn/wifi/GetIpAddressMethod.java
@@ -0,0 +1,96 @@
+package com.wwl.can.learn.wifi;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+/**
+ * 用来获取手机本身的信号来源的ip地址；
+ * Created by wangwenliang on 2017/12/5.
+ */
+
+public class GetIpAddressMethod {
+    private static String ip;
+    private static ConnectivityManager manager = null;
+
+    public static String getIpAddress(Context context){
+        manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mobileNetInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifiNetInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mobileNetInfo.isConnected()){
+            ip = getLocalIpAddress();
+        }else if (wifiNetInfo.isConnected()){
+            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int ipAddress = wifiInfo.getIpAddress();
+            ip = intToIp(ipAddress);
+        }
+        return ip;
+    }
+
+    private static String intToIp(int ipInt) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ipInt & 0xFF).append(".");
+        sb.append((ipInt >> 8) & 0xFF).append(".");
+        sb.append((ipInt >> 16) & 0xFF).append(".");
+        sb.append((ipInt >> 24) & 0xFF);
+        return sb.toString();
+    }
+
+    private static String getLocalIpAddress() {
+        try {
+            String ipv4;
+            ArrayList<NetworkInterface> nilist = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface ni: nilist)
+            {
+                ArrayList<InetAddress>  ialist = Collections.list(ni.getInetAddresses());
+                for (InetAddress address: ialist){
+                    if (!address.isLoopbackAddress() && (address instanceof Inet4Address))
+                    {
+                        ipv4 = address.getHostAddress().toString();
+                        return ipv4;
+                    }
+                }
+
+            }
+
+        } catch (SocketException ex) {
+            Log.e("localip", ex.toString());
+        }
+        return null;
+    }
+
+    private static String getlocalIp() {
+        String ip;
+
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()&&!inetAddress.isLinkLocalAddress()) {
+//                              ip=inetAddress.getHostAddress().toString();
+                        System.out.println("ip=========="+inetAddress.getHostAddress().toString());
+                        return inetAddress.getHostAddress().toString();
+
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("WifiPreference IpAddress", ex.toString());
+        }
+        return null;
+    }
+
+}
